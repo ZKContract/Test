bf582245ab9810001e866e36448057605c8681e5
QB <qb@polyhedra.network>
2024-10-23 23:30:53 +0800

    first commit

---
+++ packages/uniswap/src/data/tradingApi/__generated__/models/CreateSendRequest.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { Address } from './Address';
+import type { ChainId } from './ChainId';
+import type { TokenAmount } from './TokenAmount';
+import type { Urgency } from './Urgency';
+export type CreateSendRequest = {
+    sender: Address;
+    recipient: Address;
+    token: Address;
+    amount: TokenAmount;
+    chainId?: ChainId;
+    urgency?: Urgency;
+};
+
